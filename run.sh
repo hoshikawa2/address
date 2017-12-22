043ed8904c57dce442edde68c1ead6350d198955
choshika <choshika@CHOSHIKA-BR.mshome.net>
2017-12-22 12:02:02 -0200

    teste

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 docker pull hoshikawa2/address:latest
 docker run -it hoshikawa2/address:latest
 
-cd /pipeline/code
-
-java -jar ./code/address-0.0.1-SNAPSHOT.jar server ./address.yml
+java -jar /code/address-0.0.1-SNAPSHOT.jar server /address.yml
