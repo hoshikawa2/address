8f5a1fb5c2993bc28c08b3ff41c80864fe0fef9c
choshika <choshika@CHOSHIKA-BR.mshome.net>
2017-12-22 13:44:16 -0200

    Versao funcional

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker pull hoshikawa2/address:latest
-docker run -p 7891:7891 -t -d --name address hoshikawa2/address 
-
-java -jar /pipeline/source/target/address-0.0.1-SNAPSHOT.jar server /pipeline/source/address.yml
